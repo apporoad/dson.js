6a66091e6b2b3cef3b197fd0791b062e3cab9cb0
apporoad <apporoadina@163.com>
2020-04-25 23:27:16 +0800

    some

---
+++ demo.js
@@ -0,0 +1,33 @@
+// 这里是经典测试
+const d = D = DSON = require('./index').DSON
+const JVD = j = J = require('./index').JVD
+
+
+//var it2 = global.debug || it
+
+var fateJson = require('./docs/fate.json')
+var classroomJson = require('./docs/classroom.json')
+
+
+//  1.  找到出现在多个游戏中的servant
+
+// 2. 找到同时是 master和servant 的人
+
+// 3. 找到没有宝具的 servant
+
+//4. 找到有哪些职阶
+
+//5. 生成简要的人物介绍 
+
+//6. 找到所有游戏中的美女
+
+//7. 统计所有cv
+
+//8. 获取最敏捷的3位女性servant
+
+//9. 找到能力平均的同学
+
+//10 . 找到各班班花
+
+//11. 寻找比轻井泽惠身材更好的女性竞争者
+
